7c66f597fb8ebe8b03b62940448f8f2f4aa7db23
Peter Ferenczy <ggpeti@gmail.com>
2017-01-07 20:19:41 +0100

    add board matrix

--- Main.elm
+++ Main.elm
@@ -1,13 +1,21 @@
 import Graphics.Render exposing (..)
 import Html exposing (Html, div, span)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onMouseEnter, onMouseLeave)
 import Color exposing (..)
 import List exposing (range, map, concatMap)
+import Matrix exposing (..)
+
+type Cell = Black | White | Empty
 
 type alias Model =
-  { selection: Maybe (Int, Int) }
+  { selection : Maybe (Int, Int)
+  , board : Matrix Cell
+  }
 
-type Msg = SelectCell (Maybe (Int, Int))
+type Msg =
+    Hover (Maybe (Int, Int))
+
 
 slot_pixels : number
 slot_pixels = 20
@@ -18,40 +26,57 @@ board_size = 15
 board_pixels : number
 board_pixels = slot_pixels * board_size
 
-view_slot : Html Msg
-view_slot =
-  [ ((slot_pixels/ 2, 0), (slot_pixels/ 2, slot_pixels))
-  , ((0, slot_pixels/ 2), (slot_pixels, slot_pixels/ 2))
-  ]
-  |> map (uncurry segment)
-  |> map (solidLine 1 (solid black))
-  |> group
-  |> svg 0 0 slot_pixels slot_pixels
+
+view_isxn : Bool -> Html Msg
+view_isxn selected =
+  let
+    end = slot_pixels
+    mid = slot_pixels / 2
+    cross =
+      [ ((mid, 0), (mid, end))
+      , ((0, mid), (end, mid))
+      ]
+      |> List.map (uncurry segment)
+      |> List.map (solidLine 1 (solid black))
+    dot = circle (mid)
+          |> (filled (solid black))
+          |> position (mid, mid)
+  in svg 0 0 end end <|
+    group (cross ++ if selected then [ dot ] else [])
 
 
-view_board : Html Msg
-view_board =
-  div [ style [("width", toString board_pixels ++ "px")] ]
-    (range 1 board_size |> map (λx ->
-      div [ style [("line-height", "0")] ]
-        (range 1 board_size |> map (λy ->
-          div [ style [ ("display", "inline-block") ] ]
-            [ view_slot ]))))
+view_board : Model -> Html Msg
+view_board model =
+  div
+    [ onMouseLeave (Hover Nothing)
+    , style
+      [ ("line-height", "0")
+      , ("width", toString board_pixels ++ "px")
+      , ("background-color", "rgb(253, 226, 119)")]
+    ]
+    (range 1 board_size |> List.map (λx ->
+      div []
+        (range 1 board_size |> List.map (λy ->
+          div
+            [ onMouseEnter (Hover (Just (x, y)))
+            , style [ ("display", "inline-block") ]
+            ]
+            [ view_isxn (model.selection == Just (x, y)) ]))))
 
 view : Model -> Html Msg
 view model =
-  view_board
+  view_board model
   -- div [] [ view_board , view_board, view_board ]
 
 
 update : Msg -> Model -> Model
 update msg model = case msg of
-  SelectCell s -> { selection = s }
+  Hover h -> { model | selection = h }
 
 
 main : Program Never Model Msg
 main = Html.beginnerProgram
-  { model = { selection = Nothing }
+  { model = { selection = Nothing, board = Matrix.square board_size (always Empty) }
   , view = view
   , update = update
   }
