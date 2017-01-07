52fb74aaddbd721576e96de558b665f6d1bc508d
Peter Ferenczy <ggpeti@gmail.com>
2017-01-07 18:19:09 +0100

    define Model and Msg

--- Main.elm
+++ Main.elm
@@ -1,19 +1,24 @@
 import Graphics.Render exposing (..)
-import Html exposing (div, span)
+import Html exposing (Html, div, span)
 import Html.Attributes exposing (style)
 import Color exposing (..)
 import List exposing (range, map, concatMap)
 
+type alias Model =
+  { selection: Maybe (Int, Int) }
+
+type Msg = SelectCell (Maybe (Int, Int))
 
 slot_pixels : number
 slot_pixels = 20
+
 board_size : number
 board_size = 15
+
 board_pixels : number
 board_pixels = slot_pixels * board_size
 
-
-view_slot : Html.Html msg
+view_slot : Html Msg
 view_slot =
   [ ((slot_pixels/ 2, 0), (slot_pixels/ 2, slot_pixels))
   , ((0, slot_pixels/ 2), (slot_pixels, slot_pixels/ 2))
@@ -24,20 +29,29 @@ view_slot =
   |> svg 0 0 slot_pixels slot_pixels
 
 
-view_board : Html.Html msg
+view_board : Html Msg
 view_board =
   div [ style [("width", toString board_pixels ++ "px")] ]
     (range 1 board_size |> map (λx ->
-      div [ style [("line-height", "0")] ] 
+      div [ style [("line-height", "0")] ]
         (range 1 board_size |> map (λy ->
           div [ style [ ("display", "inline-block") ] ]
             [ view_slot ]))))
 
-view : Html.Html msg
-view =
+view : Model -> Html Msg
+view model =
   view_board
+  -- div [] [ view_board , view_board, view_board ]
 
 
+update : Msg -> Model -> Model
+update msg model = case msg of
+  SelectCell s -> { selection = s }
 
-main : Html.Html msg
-main = view
+
+main : Program Never Model Msg
+main = Html.beginnerProgram
+  { model = { selection = Nothing }
+  , view = view
+  , update = update
+  }
